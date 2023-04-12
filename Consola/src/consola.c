ecc7288f1a6281c9c57c92f69d74792f5316acc2
AgusNeus <agtineus@gmail.com>
2023-04-12 22:14:16 +0000

    Consola se conecta a Kernel.

--- Consola/src/consola.c
+++ Consola/src/consola.c
@@ -1,8 +1,35 @@
 #include "consola.h"
 
 int main (){
+	int consola;
 
-	printf("Hola mundo");
+	log_consola = log_create("consola.log","consola",1,LOG_LEVEL_TRACE);
+
+	config_consola = config_create("consola.config");
+
+	log_info(log_consola, "Se creo todo bien");
+
+	puerto = config_get_string_value(config_consola,"PUERTO_KERNEL");
+	ip = config_get_string_value(config_consola,"IP_KERNEL");
 
 
+	log_info(log_consola, "Se sacaron las cosas bien");
+
+	consola = crear_conexion(log_consola,"Kernel", ip, puerto );
+
+	log_info(log_consola, "Se creo la conexion");
+
+	terminar_consola(log_consola, consola, config_consola);
+
+	return EXIT_SUCCESS;
 }
+
+void terminar_consola(t_log* log,int conexion, t_config* config){
+	log_destroy(log);
+	//liberar_conexion(conexion);
+	config_destroy(config);
+}
+
+
+
+
--- Consola/src/consola.h
+++ Consola/src/consola.h
@@ -4,4 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../../home/utnso/tp-2023-1c-Kernel-Masters/Shared/sockets.h"
+
+
+t_config* config_consola;
+t_log* log_consola;
+char* ip;
+char* puerto;
+void terminar_consola(t_log* log ,int conexion, t_config* config);
+
 #endif
