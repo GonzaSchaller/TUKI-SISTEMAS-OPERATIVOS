d7ddb72d20892f465662ebc689d7e20b67873974
Amira Yucra <amiray@frba.utn.edu.ar>
2023-04-13 00:13:44 +0000

    agregamos los parametros que faltaban para usar los nuevos sockets

--- Cpu/src/cpu.c
+++ Cpu/src/cpu.c
@@ -1,5 +1,5 @@
 #include "cpu.h"
-//#include "shared.c"
+#include "sockets.c"
 
 
 
@@ -20,17 +20,14 @@ int main (){
 	ip = config_get_string_value(config,"IP");
 	puerto = config_get_string_value(config,"PUERTO");
 
-	//crear conexion
-	conexion = crear_conexion(ip,puerto);
+	//crear conexion para memoria
+	conexion = crear_conexion(logger,"Memoria",ip,puerto);
 
 	//cuando el cpu es server del kernel
-	int cliente = esperar_cliente();
+	//problema de index, arreglar
+	int cliente = esperar_cliente(logger,server_fd);
 
-
-
-
-	terminar_programa(conexion, logger, config);
-
+	terminar_programa(server_fd,conexion, logger, config);
 
 	//para recv()
 	uint32_t handshake = 1;
@@ -38,58 +35,6 @@ int main (){
 
 
 
+
 }
 
-
-//PASARLO A SHARED
-
-t_log* iniciar_logger(void)
-{
-	t_log* nuevo_logger;
-	nuevo_logger = log_create("CPU.log", "CPU", 1, LOG_LEVEL_INFO);
-
-	if(nuevo_logger == NULL){
-		puts("No se pudo crear el logger");
-		exit(1);
-	}
-
-	return nuevo_logger;
-}
-
-int crear_conexion(char *ip, char* puerto)
-{
-	struct addrinfo hints;
-	struct addrinfo *server_info;
-
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
-
-	getaddrinfo(ip, puerto, &hints, &server_info);
-
-	// Ahora vamos a crear el socket.
-	int socket_cliente = socket(server_info->ai_family,
-								server_info->ai_socktype,
-								server_info->ai_protocol);
-
-	// Ahora que tenemos el socket, vamos a conectarlo
-	connect(socket_cliente, server_info->ai_addr,server_info->ai_addrlen);
-
-	freeaddrinfo(server_info);
-
-	return socket_cliente;
-}
-
-int esperar_cliente(int socket_servidor)
-{
-	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	//assert(!"no implementado!");
-
-	// Aceptamos un nuevo cliente
-	int socket_cliente = accept(socket_servidor,NULL, NULL);
-
-	log_info(logger, "Se conecto un cliente!");
-
-	return socket_cliente;
-}
