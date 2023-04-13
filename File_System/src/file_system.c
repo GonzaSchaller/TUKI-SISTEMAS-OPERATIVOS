64b2fb7c7ada2f0a6fb210b164f9bf84dd1104d5
Amira Yucra <amiray@frba.utn.edu.ar>
2023-04-13 00:15:20 +0000

    creamos la conexion y esperamos al cliente

--- File_System/src/file_system.c
+++ File_System/src/file_system.c
@@ -1,8 +1,31 @@
 #include "file_system.h"
 
 int main (){
+	int conexion;
+	char* ip;
+	char* puerto;
 
-	printf("Hola mundo");
+	//crear un logger
+	t_log* logger;
+	logger = iniciar_logger();
+
+	//config
+	t_config* config;
+	config = iniciar_config();
+
+	//lo de la ip y puerto
+	ip = config_get_string_value(config,"IP");
+	puerto = config_get_string_value(config,"PUERTO");
+
+	//crear conexion para memoria
+	conexion = crear_conexion(logger,"Memoria",ip,puerto);
+
+	//cuando el cpu es server del kernel
+	//problema de index, arreglar
+	int cliente = esperar_cliente(logger,server_fd);
+
+	terminar_programa(server_fd,conexion, logger, config);
+
 
 
 }
