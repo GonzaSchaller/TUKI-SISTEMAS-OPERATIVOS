92acd8bfc34bd5d7a32390e63432498bab44250c
Amira Yucra <80787142+AmiraYucra@users.noreply.github.com>
2023-06-01 09:07:21 -0300

    Update cpu.h

--- Cpu/src/cpu.h
+++ Cpu/src/cpu.h
@@ -10,25 +10,34 @@
 #include <readline/readline.h>
 #include <sockets.h>
 
-#include<signal.h>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<netdb.h>
-#include<string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
 #include "configuraciones.h"
 #include <pthread.h>
+#include "send_rcvs.h"
+
+extern t_log* logger;
+extern t_config* config;
 
 extern int conexion;
-extern t_config* config;
-extern t_log* logger;
-
-void iniciar_config(t_config* );
+/*
+extern char* ip;
+extern char* puerto_cpu;
+extern char* ip_memoria;
+extern char* puerto_memoria;
+extern char* retardo_instruccion;
+extern char* tam_max_segmento;
+*/
 
 t_log* iniciar_logger(void);
 t_config* iniciar_config(void);
+void levantar_config();
 void leer_consola(t_log*);
 void paquete(int);
 void terminar_programa(int, t_log*, t_config*);
-
+void recibir_instrucciones();
 
 #endif
