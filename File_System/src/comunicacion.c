973d9bc4dba693860079512eee15080eada87804
agtorres1 <agtorres@frba.utn.edu.ar>
2023-07-14 16:39:47 +0000

    codigo de ftruncate sin revisar :|

--- File_System/src/comunicacion.c
+++ File_System/src/comunicacion.c
@@ -17,7 +17,7 @@ void procesar_peticiones(int cliente_socket){
 				// DEFINIR: ver como pasar los datos nombre archivo y tamanio porque despues lo usamos en config_set_value que sólo usa punteros
 
 				//caso de F_OPEN primero hay que ver si ya existe el archivo
-				case EXISTE_ARCHIVO:
+				case EXISTE_ARCHIVO:{
 					extra_code estado;
 					char*nombre_archivo;
 					uint32_t tamanio; // DEFINIR ver si cambiarlo a char* o castear
@@ -40,24 +40,24 @@ void procesar_peticiones(int cliente_socket){
 						estado = CORRECTO;
 						send_OK_CODE(cliente_socket, estado);
 					}
-
+				}
 				break;
 
 
-				case F_WRITE:
+				case F_WRITE:{
 					char* nombre_archivow;
-				    uint32_t dfw; // la DF
-					uint32_t cbw; // cant bytes
+				    uint32_t df; // la DF
+					uint32_t cant_bytes; // cant bytes
 					char*contenido;
 					uint32_t punterow = 0;
 					uint32_t estadok;
 
-					recv_F_WRITE(cliente_socket,&nombre_archivow,&dfw,&cbw);
+					recv_F_WRITE(cliente_socket,&nombre_archivow,&df,&cant_bytes);
 
-					log_info(logger,"Escribir Archivo: <%s> - Puntero: <%d> - Memoria <%d> - Tamanio: <%d>",nombre_archivow,punterow,dfw,cbw);
+					log_info(logger,"Escribir Archivo: <%s> - Puntero: <%d> - Memoria <%d> - Tamanio: <%d>",nombre_archivow,punterow,df,cant_bytes);
 
 					//solicito a memoria lo que hay en la direccion logica mandada
-					send_READ2(fd_memoria, dfw);
+					send_READ2(fd_memoria, df);
 					//aca recibo el contenido que le pediu a memoria
 					recv_contenido_leido(fd_memoria,&contenido);
 					//recibo un puntero?
@@ -66,19 +66,19 @@ void procesar_peticiones(int cliente_socket){
 						estadok = EXITOSO; //escribio bien
 						send_OK_CODE(cliente_socket,estadok);
 					}
-
+				   }
 					   break;
-				case F_READ:
+				case F_READ:{
 					uint32_t estado_memoria;
 					uint32_t estado_kernel;
-					char* nombre_archivor;
+					char* nombre_archivo;
 					uint32_t df;
-					uint32_t cb; //cant bytes
+					uint32_t cant_bytes;
 					uint32_t puntero=0;
-					recv_F_READ(cliente_socket,&nombre_archivor,&df,&cb);
+					recv_F_READ(cliente_socket,&nombre_archivo,&df,&cant_bytes);
 
-					log_info(logger,"Leer: Archivo: %s - Puntero: %d  - Memoria: <%d>  - Tamanio: <%d>",nombre_archivor,puntero,df,cb);
-					char*contenidor = buscar_contenido(puntero,cb);
+					log_info(logger,"Leer: Archivo: %s - Puntero: %d  - Memoria: <%d>  - Tamanio: <%d>",nombre_archivo,puntero,df,cant_bytes);
+					char*contenidor = buscar_contenido(puntero,cant_bytes);
 					//le mando a memoria lo que tiene que escribir
 					if(contenidor != NULL){
 						send_WRITE(fd_memoria,df,contenidor);
@@ -94,11 +94,61 @@ void procesar_peticiones(int cliente_socket){
 					log_error(logger, "Error al abrir el archivo por que no se pudo obtener el contenido del mismo");
 
 						break;
+				}
 
-				case F_TRUNCATE:
-					char* nombre_archivo_truncate; //TODO DUDAS como es una variable deberia ser una general en vez de hacer varias para cada case
-					uint32_t tamanio_truncate;
+				case F_TRUNCATE:{
+					char* nombre_archivo; //TODO DUDAS como es una variable deberia ser una general en vez de hacer varias para cada case
+					uint32_t tamanio;
 					recv_F_TRUNCATE(cliente_socket, &nombre_archivo, &tamanio);
+
+					char* path = concat(nombre_archivo); //TODO DUDAS (CONCAT): ver si nombre_archivo va asÃ­ o con el * o &
+					t_config* archivo = config_create(path);
+					int pd = config_get_int_value(archivo, "PUNTERO_DIRECTO");
+					int pi = config_get_int_value(archivo, "PUNTERO_INDIRECTO");
+
+					uint32_t tamanio_archivo = config_get_int_value(archivo, "TAMANIO_ARCHIVO");
+					if(tamanio > tamanio_archivo){
+
+					uint32_t cantidadBloques = (uint32_t)ceil((double)tamanio / superbloque->block_size); //bloques a agregar
+					for(int i = 0; i<bitarray_get_max_bit(bitarray); i++){
+						uint32_t bloques_asignados = 0;
+						if(!bitarray_test_bit(bitarray, i) && bloques_asignados != cantidadBloques){ // si el bit es 0 y todavia me faltan bloques por asignar
+							bitarray_set_bit(bitarray, i);
+							char buffer[5];
+							sprintf(buffer, "%d", i); //chequear
+
+						if(pd == -1){ // no tiene puntero directo asginado
+						config_set_value(archivo,"PUNTERO_DIRECTO",buffer);
+						}
+						else if(pi == -1){ //no tiene puntero indirecto
+							config_set_value(archivo,"PUNTERO_INDIRECTO",buffer);
+						}
+						else if(pi != -1){ //ya tiene un puntero indirecto
+						//hay que ver que puntero es y agregarle un puntero a un bloque (Ni idea como hacerlo)
+												}
+							bloques_asignados++;
+						}
+						}
+						}
+							else{ //es mas chico
+								int tamanioASacar = tamanio_archivo - tamanio;
+								int bloquesASacar = (uint32_t)ceil((double)tamanioASacar / superbloque->block_size); //bloques a agregar
+
+								for(int i = 0; i<bitarray_get_max_bit(bitarray); i++){
+											uint32_t bloques_sacados = 0;
+											if(bloques_sacados != bloquesASacar){
+												//me fijo el puntero indirecto, entro al bloque y me fijo la cantidad de punteros a bloques y cuales son
+												// voy sacando uno por uno del bitmap -> bitarray_clean_bit
+												bloques_sacados++;
+											}
+										}
+
+									}
+									config_set_value(archivo,"TAMANIO_ARCHIVO",tamanio);
+
+										break;
+
+								}
 
 					//uso funcion concat para obtener el path y asi usar c
 					//TODO DUDAS no se como llegar al FCB
@@ -111,6 +161,7 @@ void procesar_peticiones(int cliente_socket){
 						// Si es más grande que el de antes:
 							//TODO Asignar nuevos bloques (ver que significa)
 						break;
+				}
 
 				}
 
