71298f0c67307ab9af0d285bb6eea2c25821fe3c
Gonzalo Schaller <102746916+GonzaSchaller@users.noreply.github.com>
2023-07-29 05:09:01 -0300

    Delete prueba_BASE.sh

--- Consola/prueba_BASE.sh
+++
@@ -1,5 +0,0 @@
-./consola.out consola.config /home/utnso/tp-2023-1c-Kernel-Masters/Pruebas/BASE_1 &
-sleep 1
-./consola.out consola.config /home/utnso/tp-2023-1c-Kernel-Masters/Pruebas/BASE_2 &
-sleep 1
-./consola.out consola.config /home/utnso/tp-2023-1c-Kernel-Masters/Pruebas/BASE_2 
