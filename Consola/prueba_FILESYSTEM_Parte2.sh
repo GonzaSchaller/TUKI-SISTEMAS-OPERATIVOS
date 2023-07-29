7868b0cca100ae96d828d5825c3d21cd537a01bf
Gonzalo Schaller <102746916+GonzaSchaller@users.noreply.github.com>
2023-07-29 05:10:07 -0300

    Delete prueba_FILESYSTEM_Parte2.sh

--- Consola/prueba_FILESYSTEM_Parte2.sh
+++
@@ -1,9 +0,0 @@
-./consola.out consola.config /home/utnso/tp-2023-1c-Kernel-Masters/Pruebas/FS_3 &
-
-./consola.out consola.config /home/utnso/tp-2023-1c-Kernel-Masters/Pruebas/FS_3 &
-
-./consola.out consola.config /home/utnso/tp-2023-1c-Kernel-Masters/Pruebas/FS_3 &
-
-./consola.out consola.config /home/utnso/tp-2023-1c-Kernel-Masters/Pruebas/FS_3 
-
-
