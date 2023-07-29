ef2e40778f75317d0fd23cb5b0d5224439b7975d
Gonzalo Schaller <102746916+GonzaSchaller@users.noreply.github.com>
2023-07-29 05:41:10 -0300

    Update funcionesAuxiliares.c

--- Memoria/src/funcionesAuxiliares.c
+++ Memoria/src/funcionesAuxiliares.c
@@ -66,7 +66,7 @@ bool seg_con_base_igual(void* segmento){
 segmento_t* encontrar_base_tso(uint32_t base){
 	base_a_buscar = base;
 	segmento_t* seg = (segmento_t*) list_find(segmentos_ocupados,&seg_con_base_igual);
-	log_info(log_memoria,"segmento encontrado : base %d , id %d , tamanio %d ", seg->direccion_Base,seg->id,seg->tamanio);
+	log_info(log_memoria,"Segmento encontrado : base %d , id %d , tamanio %d ", seg->direccion_Base,seg->id,seg->tamanio);
 	return seg;
 }
 void remover_segmento_entso(uint32_t id){
@@ -151,7 +151,7 @@ void eliminar_pid_lista_pids(uint32_t pid){
 	pid_a_buscar = pid;
 	list_remove_and_destroy_by_condition(lista_de_pids,&pid_igual,free);
 	uint32_t size = list_size(lista_de_pids);
-	log_info(log_memoria,"size lista pids %d",size);
+	log_info(log_memoria,"Size lista pids %d",size);
 	for(int i =0;i<size;i++){
 		uint32_t *pid = list_get(lista_de_pids,i);
 		log_info(log_memoria,"pid %u",*pid);
@@ -160,9 +160,9 @@ void eliminar_pid_lista_pids(uint32_t pid){
 //0 libres, 1 ocupados
 void mostrar_tsl_actualizado(t_list*lista,uint32_t cual_es){
 	if(cual_es == 0){
-		log_info(log_memoria,"lista de segmentos libres actualizada");
-	}else if(cual_es ==1) {log_info(log_memoria,"lista de segmentos ocupados actualizada");}
-	else log_info(log_memoria,"tabla de segmentos encontrados donde entra el tamanio solicitado: ");
+		log_info(log_memoria,"Lista de segmentos libres actualizada");
+	}else if(cual_es ==1) {log_info(log_memoria,"Lista de segmentos ocupados actualizada");}
+	else log_info(log_memoria,"Tabla de segmentos encontrados donde entra el tamanio solicitado: ");
 	uint32_t size = list_size(lista);
 
 	for(int i=0;i<size;i++){
