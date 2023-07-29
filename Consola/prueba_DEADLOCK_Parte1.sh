e9b5a3b0d2601a649037d56b8a72635809815b3e
Gonzalo Schaller <102746916+GonzaSchaller@users.noreply.github.com>
2023-07-29 04:50:40 -0300

    Delete prueba_DEADLOCK_Parte1.sh

--- Consola/prueba_DEADLOCK_Parte1.sh
+++
@@ -1,5 +0,0 @@
-./consola.out consola.config /home/utnso/tp-2023-1c-Kernel-Masters/Pruebas/DEADLOCK_1 &
-
-./consola.out consola.config /home/utnso/tp-2023-1c-Kernel-Masters/Pruebas/DEADLOCK_2 &
-
-./consola.out consola.config /home/utnso/tp-2023-1c-Kernel-Masters/Pruebas/DEADLOCK_3
