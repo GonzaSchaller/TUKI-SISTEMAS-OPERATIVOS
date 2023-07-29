edb475b66454a34779eb3d7fcdb99600a0e28664
Gonzalo Schaller <102746916+GonzaSchaller@users.noreply.github.com>
2023-07-29 05:07:46 -0300

    Delete prueba_MEMORIA.sh

--- Consola/prueba_MEMORIA.sh
+++
@@ -1,5 +0,0 @@
-./consola.out consola.config /home/utnso/tp-2023-1c-Kernel-Masters/Pruebas/MEMORIA_1 &
-sleep 0.5
-./consola.out consola.config /home/utnso/tp-2023-1c-Kernel-Masters/Pruebas/MEMORIA_2 &
-sleep 0.5
-./consola.out consola.config /home/utnso/tp-2023-1c-Kernel-Masters/Pruebas/MEMORIA_3
