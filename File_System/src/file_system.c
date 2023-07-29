b7cc69944e3fc2420fb0e853666045e6f89c48c4
Amira Yucra <80787142+AmiraYucra@users.noreply.github.com>
2023-07-29 16:13:46 -0300

    Update file_system.c

--- File_System/src/file_system.c
+++ File_System/src/file_system.c
@@ -10,7 +10,7 @@ int main (){
 
 	levantar_config();
 
-	fd_fs = iniciar_servidor(logger, "fileSystem", ip, c->puerto_escucha);
+	fd_fs = iniciar_servidor(logger, "fileSystem", "192.168.1.50", c->puerto_escucha);
 	generar_conexion_con_memoria();
 	
 	cargar_superbloque();
